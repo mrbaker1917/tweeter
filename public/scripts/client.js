2df0e6eed4d10af38e6303650a151c0d7b2a82df
Mark Baker <mrbaker1917@gmail.com>
2020-09-09 10:13:08 -0700

    adds createTweetElement to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,11 +5,46 @@
  */
 
  // shows user's handle on mouseover tweet
-$(document).ready(() => {
-  $("article.tweet").mouseenter((event) => {
+$(() => {
+  $("article.tweet").mouseenter(function(event) {
     $(".handle").addClass("handleShow");
   });
-  $("article.tweet").mouseleave((event) => {
+  $("article.tweet").mouseleave(function(event) {
     $(".handle").removeClass("handleShow");
   });
+
+  //function that takes in a tweet object and returns a tweet article element
+  const createTweetElement = function(tweetObj) {
+    const createdAt = tweetObj.created_at;
+    const currentDate = Date.now();
+    const daysSince = Math.round((currentDate - createdAt) / 1000 / 3600 / 24);
+    let $tweet = $(`
+    <article class="tweet">
+    <header>
+      <div class="avatar">
+        <img src=${tweetObj.user.avatars} />
+        <span class="name">${tweetObj.user.name}</span>
+      </div>
+      <span class="handle">${tweetObj.user.handle}</span>
+    </header>
+    <p class="tweetText">${tweetObj.content.text}</p>
+    <footer>
+      <span class="daysSince">${daysSince} days ago</span>
+      <div id="icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+    `);
+    return $tweet;
+  };
+  const renderTweets = function(data) {
+    for (const tweet of data) {
+      createTweetElement(tweet);
+    }
+  };
 });
+
+
