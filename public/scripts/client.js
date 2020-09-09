461052faa876fbed6b4262c1ada0dc5d0a49f6ae
Mark Baker <mrbaker1917@gmail.com>
2020-09-08 18:09:50 -0700

    styles tweet-template and adds shadow box and handle

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  $("article.tweet").mouseenter((event) => {
+    $("#handle").css("opacity", "1.0");
+  });
+  $("article.tweet").mouseleave((event) => {
+    $("#handle").css("opacity", "0.1");
+  });
+});
