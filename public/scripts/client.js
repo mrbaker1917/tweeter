590c2624d6ffb2c8e5c4b0fe2858c0141102d1b0
Mark Baker <mrbaker1917@gmail.com>
2020-09-09 11:58:26 -0700

    fixes handle appearing prob for multiple tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,14 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- // shows user's handle on mouseover tweet
-$(() => {
-  $("article.tweet").mouseenter(function(event) {
-    $(".handle").addClass("handleShow");
-  });
-  $("article.tweet").mouseleave(function(event) {
-    $(".handle").removeClass("handleShow");
-  });
+// shows user's handle on mouseover tweet
+$(document).ready(function() {
 
   //function that takes in a tweet object and returns a tweet article element
   const createTweetElement = function(tweetObj) {
@@ -20,26 +14,39 @@ $(() => {
     const daysSince = Math.round((currentDate - createdAt) / 1000 / 3600 / 24);
     let $tweet = $(`
     <article class="tweet">
-    <header>
-      <div class="avatar">
-        <img src=${tweetObj.user.avatars} />
-        <span class="name">${tweetObj.user.name}</span>
-      </div>
-      <span class="handle">${tweetObj.user.handle}</span>
-    </header>
-    <p class="tweetText">${tweetObj.content.text}</p>
-    <footer>
-      <span class="daysSince">${daysSince} days ago</span>
-      <div id="icons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </div>
-    </footer>
-  </article>
+      <header>
+        <div class="avatar">
+          <img src=${tweetObj.user.avatars} />
+          <span class="name">${tweetObj.user.name}</span>
+        </div>
+        <span class="handle">${tweetObj.user.handle}</span>
+      </header>
+      <p class="tweetText">${tweetObj.content.text}</p>
+      <footer>
+        <span class="daysSince">${daysSince} days ago</span>
+        <div id="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>
     `);
     return $tweet;
   };
+  const tweet = {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+  $("#tweet-container").append(createTweetElement(tweet));
+
   const renderTweets = function(data) {
     for (const tweet of data) {
       createTweetElement(tweet);
